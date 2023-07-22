006baff030af5b740d18734c85b6b26cd4298bf6
kyairlakayla <kyairlakayla.davis>
2023-07-21 23:51:58 -0400

    add query options

--- server.js
+++ server.js
@@ -2,9 +2,42 @@ const { animals } = require('./data/animals');
 const express = require('express');
 const app = express();
 
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    let personalityTraitsArray = [];
+
+if (query.personalityTraits) {
+    if (typeof query.personalityTraits === 'string') {
+        personalityTraitsArray = [query.personalityTraits];
+    } else {
+        personalityTraitsArray = query.personalityTraits;
+    }
+    personalityTraitsArray.forEach(trait => {
+        filteredResults = filteredResults.filter(
+            animal => animal.personalityTraits.indexOf(trait) !== -1
+        );
+    });      
+}
+
+if (query.diet) {
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+}
+if (query.specis) {
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+}
+if (query.name) {
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+}
+
+return filteredResults;
+}
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
+    
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
     console.log(req.query)
 
     res.json(results);
